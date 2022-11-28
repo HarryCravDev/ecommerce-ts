f3403788bb5865cdee7fe0d152e26e8cbfe5ac7e
Harry Craven <harry.craven@ims-evolve.com>
2022-11-28 19:51:39 +0000

    Adding redux store and project test data.

--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,13 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import { store } from "./redux/store/store";
+import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
-		<App />
+		<Provider store={store}>
+			<App />
+		</Provider>
 	</React.StrictMode>
 );
