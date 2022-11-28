2f1a4dc5e00f614d01e67bcbb21c29c4b52f8fc3
Harry Craven <harry.craven@ims-evolve.com>
2022-11-28 19:52:12 +0000

    Adding footer component.

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Container from '@mui/material/Container';
+
+const Footer = () => {
+  return (
+    <footer>
+        <Container>
+            <div className='text-center'>
+                <p>Copyright &copy; ProShop</p>
+            </div>
+        </Container>
+    </footer>
+  )
+}
+
+export default Footer
